5a5def54a84c86b46c40262db66eda6d4bfdf15d
Shibby <103569393+ShibbyRJ@users.noreply.github.com>
2022-09-08 12:49:04 +0100

    First Commit

---
+++ NewFileCopier/NewFileCopierConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewFileCopier
+{
+    internal class NewFileCopierConfig
+    {
+        public List<Mapping> Mappings { get; set; }
+    }
+
+
+    internal class Mapping
+    {
+        public string SourcePath { get; set; }
+        public string DestinationPath { get; set; }
+        public int DelayBeforeCopy { get; set; } = 20;
+    }
+}
---
+++ NewFileCopier/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using NewFileCopier;
+using System.Runtime.CompilerServices;
+
+NewFileCopierConfig _config;
+List<FileSystemWatcher> _watchers;
+
+Initialise();
+
+JoinTheWatch();
+
+void JoinTheWatch()
+{
+    foreach (var mapping in _config.Mappings)
+    {
+        FileSystemWatcher watcher = new FileSystemWatcher(mapping.SourcePath);
+        watcher.Created += new FileSystemEventHandler((s, e) => OnCreated(s, e, mapping));
+        watcher.EnableRaisingEvents = true;
+        _watchers.Add(watcher);
+    }
+
+    Console.WriteLine("Always watching, Wazowski.");
+    Console.ReadLine();
+}
+
+static void OnCreated(object sender, FileSystemEventArgs e, Mapping mapping)
+{
+    Console.WriteLine($"Created: {e.FullPath}. Waiting for {mapping.DelayBeforeCopy}s...");
+    Thread.Sleep(mapping.DelayBeforeCopy * 1000);
+    string destination = Path.Combine(mapping.DestinationPath, e.Name ?? Path.GetFileName(e.FullPath));
+    Console.WriteLine($"Copying to {destination}...");
+    File.Copy(e.FullPath, destination);
+    Console.WriteLine("Done");
+}
+
+void Initialise()
+{
+    string configFilename = "appSettings.json";
+    IConfigurationBuilder builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(configFilename);
+
+    IConfiguration configRoot = builder.Build();
+
+    _config = new();
+    configRoot.Bind(_config);
+
+    _watchers = new List<FileSystemWatcher>();
+}
